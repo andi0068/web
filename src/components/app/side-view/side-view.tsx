ae761d6e04aa07904ccfdda0003706cb5bc0afa5
Andi <andideve@outlook.com>
2023-12-18 15:00:52 +0700

    style(side-view): remove the border and set bg to secondary

--- src/components/app/side-view/side-view.tsx
+++ src/components/app/side-view/side-view.tsx
@@ -1,9 +1,17 @@
+import { cn } from '@/lib/utils';
+
 import * as Base from '../view';
+import { BEM_CLASS } from './side-view.const';
 import type { BaseProps } from './side-view.types';
 
 export function Root({ children, className }: BaseProps) {
   return (
-    <Base.Root as="div" scrollbar="minimize" size="side" border="right" className={className}>
+    <Base.Root
+      as="div"
+      scrollbar="minimize"
+      size="side"
+      className={cn(`${BEM_CLASS} bg-background-secondary`, className)}
+    >
       {children}
     </Base.Root>
   );
