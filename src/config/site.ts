3497dc320f3fb7a5efd9d3791f08d3fdbc394e9c
Andi <andideve@outlook.com>
2023-12-20 07:23:10 +0700

    chore: update `siteConfig`

--- src/config/site.ts
+++ src/config/site.ts
@@ -3,6 +3,6 @@ import { env } from '@/utils/generic-utils';
 const { AUTHOR_USERNAME } = env();
 
 export const siteConfig = {
-  name: `@${AUTHOR_USERNAME}`,
-  description: `@${AUTHOR_USERNAME} profile.`,
+  name: AUTHOR_USERNAME,
+  description: `${AUTHOR_USERNAME} profile.`,
 };
