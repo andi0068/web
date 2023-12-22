0706974423f06024c3fdab0c2937e5fdf0a4a208
Andi <andideve@outlook.com>
2023-12-22 09:35:07 +0700

    refactor(app): use `Section` builder on `Nav`

--- src/components/app/_nav.tsx
+++ src/components/app/_nav.tsx
@@ -1,5 +1,5 @@
 'use client';
-import { Fragment, useMemo } from 'react';
+import React, { Fragment, useMemo } from 'react';
 import { FiPlus, FiFolder } from 'react-icons/fi';
 
 import ContextMenu from '@/components/context-menu';
@@ -12,55 +12,44 @@ import * as Actions from './view/header/actions';
 
 import { useEvents } from './_hooks';
 
-interface AuthorOnlyBtnMenuProps {
+interface SectionProps {
+  name: string;
+  children?: React.ReactNode;
+  action?: React.ReactElement;
+}
+interface FolderAuthorCtxMenuProps {
   children: React.ReactElement;
   id: string;
 }
 
 /**
  * Features
- * - Create a new folder (author only)
- * - Select and Active state
- * - Context Menu
- *    - Rename folder (author only)
- *    - Delete folder (author only)
- *    - Create a new note inside (author only)
+ * - "Folders" Section
+ *   - Select and Active state
+ *   - Create a new folder (author only)
+ *   - Context Menu (author only)
+ *     - Rename folder
+ *     - Delete folder
+ *     - Create a new note inside
  */
 export default function Nav() {
   return (
     <Base.Root>
-      <Base.Section.Root>
-        <Header />
-        <Base.Section.List.Container>
-          <List />
-        </Base.Section.List.Container>
-      </Base.Section.Root>
+      <Section name="Folders" action={<FoldersAction />}>
+        <FoldersList />
+      </Section>
     </Base.Root>
   );
 }
 
-function Header() {
+// Sections ***************************************************************************************
+
+function FoldersAction() {
   const state = useAppState();
-  return (
-    <Base.Section.Header.Root>
-      <Base.Section.Header.Heading>Folders</Base.Section.Header.Heading>
-      {state.auth.user && <AuthorOnlyActions />}
-    </Base.Section.Header.Root>
-  );
+  return <>{state.auth.user ? <CreateFolderAuthorAction /> : null}</>;
 }
 
-function AuthorOnlyActions() {
-  const ev = useEvents();
-  return (
-    <Actions.Provider size="lg">
-      <Actions.Root>
-        <Actions.Action label="Create a new folder" icon={FiPlus} onClick={ev.onCreateFolder} />
-      </Actions.Root>
-    </Actions.Provider>
-  );
-}
-
-function List() {
+function FoldersList() {
   const state = useAppState();
   const menu = useMenu('folders');
 
@@ -79,7 +68,11 @@ function List() {
         );
         return (
           <Fragment key={folder.id}>
-            {state.auth.user ? <AuthorOnlyBtnMenu id={folder.id}>{row}</AuthorOnlyBtnMenu> : row}
+            {state.auth.user ? (
+              <FolderAuthorCtxMenu id={folder.id}>{row}</FolderAuthorCtxMenu>
+            ) : (
+              row
+            )}
           </Fragment>
         );
       })}
@@ -87,7 +80,16 @@ function List() {
   );
 }
 
-function AuthorOnlyBtnMenu({ children, id }: AuthorOnlyBtnMenuProps) {
+// Actions ****************************************************************************************
+
+function CreateFolderAuthorAction() {
+  const ev = useEvents();
+  return <Actions.Action label="Create a new folder" icon={FiPlus} onClick={ev.onCreateFolder} />;
+}
+
+// Context Menus **********************************************************************************
+
+function FolderAuthorCtxMenu({ children, id }: FolderAuthorCtxMenuProps) {
   const ev = useEvents();
   const factory = useMenuFactory(
     {
@@ -115,4 +117,22 @@ function AuthorOnlyBtnMenu({ children, id }: AuthorOnlyBtnMenuProps) {
   );
 
   return <ContextMenu items={items}>{children}</ContextMenu>;
+}
+
+// Component Builders *****************************************************************************
+
+function Section({ children, name, action }: SectionProps) {
+  return (
+    <Base.Section.Root>
+      <Base.Section.Header.Root>
+        <Base.Section.Header.Heading>{name}</Base.Section.Header.Heading>
+        {action && (
+          <Actions.Provider size="lg">
+            <Actions.Root>{action}</Actions.Root>
+          </Actions.Provider>
+        )}
+      </Base.Section.Header.Root>
+      {children}
+    </Base.Section.Root>
+  );
 }
