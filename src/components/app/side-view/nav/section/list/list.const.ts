1bd6ad9219ea49b5cfd2b2b3d25390ea5200909f
Andi <andideve@outlook.com>
2023-12-25 10:32:51 +0700

    feat(list): add support for `activeBg` variants

--- src/components/app/side-view/nav/section/list/list.const.ts
+++ src/components/app/side-view/nav/section/list/list.const.ts
@@ -1,1 +1,7 @@
+import type { RowOptions } from './list.types';
+
 export const BEM_CLASS = 'c-app__side-view__nav__section__list';
+
+export const ROW_DEFAULTS: Required<RowOptions> = {
+  activeBg: 'gray',
+};
